6a5a6cb2019e20fc7ded06257dac34dfd15b35ea
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-09-02 13:54:38 +0530

    event,functions and state

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,41 @@
+"use client"
+import { useState } from "react";
 import styles from "./page.module.css";
 
+
 export default function Home() {
+  const [name,setName]=useState("Abhilash")
+  const apple=(item)=>{
+    // alert(item);
+    setName(item)
+  }
+  const Innercomponent=()=>{
+    return (
+      <h1>Inner Component</h1>
+    )
+  }
   return (
     <main className={styles.main}>
-      <h1>Home Page</h1>
       {/* --Video1--  */}
-      <User name="Abhilash"/>
+      {/* <h1>Home Page</h1> */}
+      {/* <User name="Abhilash"/>
       <User name="Aakash"/>
       <User name="Sachin"/>
-      <User name="Anil"/>
+      <User name="Anil"/> */}
+      <h1>Events,functions and state {name}</h1>
+       <button onClick={()=>apple("Anil")}>Click Me</button>
+       {/* <Innercomponent/> */}
+       {/* call component as function  */}
+       {Innercomponent()}
     </main>
   );
 }
- const User=(props)=>{
- return(
-  <div>
-    Username is {props.name}
-  </div>
- )
-}
+
+
+//  const User=(props)=>{
+//  return(
+//   <div>
+//     Username is {props.name}
+//   </div>
+//  )
+// }
