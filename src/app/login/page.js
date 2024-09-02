56b52162d58332b016b425f95acd4c3454e48ec5
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-09-02 18:28:11 +0530

    linking and navigation

---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+ const Login=()=>{
+    return (
+        <div>
+            <h1>Login page</h1>
+            <Link href="/">GO to Home Page</Link>
+        </div>
+    )
+}
+export default Login;
--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,46 @@
-"use client"
-import { useState } from "react";
+'use client'
 import styles from "./page.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 
 
 export default function Home() {
-  const [name,setName]=useState("Abhilash")
-  const apple=(item)=>{
-    // alert(item);
-    setName(item)
-  }
-  const Innercomponent=()=>{
-    return (
-      <h1>Inner Component</h1>
-    )
-  }
+  const router = useRouter()
+  // const [name,setName]=useState("Abhilash")
+  // const apple=(item)=>{
+  //   // alert(item);
+  //   setName(item)
+  // }
+  // const Innercomponent=()=>{
+  //   return (
+  //     <h1>Inner Component</h1>
+  //   )
+  // }
   return (
-    <main className={styles.main}>
+    <main className>
       {/* --Video1--  */}
       {/* <h1>Home Page</h1> */}
       {/* <User name="Abhilash"/>
       <User name="Aakash"/>
       <User name="Sachin"/>
       <User name="Anil"/> */}
-      <h1>Events,functions and state {name}</h1>
-       <button onClick={()=>apple("Anil")}>Click Me</button>
+      {/* <h1>Events,functions and state {name}</h1>
+       <button onClick={()=>apple("Anil")}>Click Me</button> */}
        {/* <Innercomponent/> */}
        {/* call component as function  */}
-       {Innercomponent()}
+       {/* {Innercomponent()}  */}
+       <h1>Basic Routing | Make New page</h1>
+       <Link href="/login">Go to Login Page</Link>
+       <br/>
+       <br/>
+       <Link href="/about">Go to About Page</Link>
+       <br/>
+       <br/>
+       <button onClick={()=>router.push("/login")}>Go to Login Page</button>
+       <br/>
+       <br/>
+       <button onClick={()=>router.push("/about")}>Go to About Page</button>
     </main>
   );
 }
